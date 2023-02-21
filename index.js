25906fc1e881a5ac1dced8d288f230d03da2aa76
Nolan Welch <bugygamer.mc@gmail.com>
2023-02-21 11:16:37 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(e){
+    const changeText = document.querySelector('p')
+    console.log (changeText)
+    changeText.innerHTML = "This is really cool!"
+    console.log (changeText)
+});
+//const changeText = document.querySelector('#text')
+    //changeText.innerText = "This is really cool!"
